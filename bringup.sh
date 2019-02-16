eaccf76846ae33273a1c0cde5114ae4798b9dbe0
kruftindustries <kd0ohr@gmail.com>
2019-02-16 17:11:16 +0800

    Update bringup.sh

--- bringup.sh
+++ bringup.sh
@@ -3,11 +3,11 @@
 apt-mark hold libgl1 libglx0 libgles2 libegl1 libglvnd0
 mkdir /etc/OpenCL && mkdir /etc/OpenCL/vendors
 echo "/usr/lib/aarch64-linux-gnu/libOpenCL.so" | tee /etc/OpenCL/vendors/armocl.icd
-apt update && apt install -y htop nano git subversion
+apt update && apt install -y htop nano git subversion cmake gparted libusb-1.0-0-dev libxrandr-dev x11proto-xinerama-dev libxinerama-dev libxcursor-dev
 fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && echo "/swapfile swap swap defaults 0 0" | tee /etc/fstab
 
 #realsense stuff
-git clone https://github.com/IntelRealSense/librealsense.git
-cd librealsense && git checkout tags/v2.18.1
+#git clone https://github.com/IntelRealSense/librealsense.git && cd librealsense && git checkout tags/v2.18.1
 #cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 #udevadm control --reload-rules && udevadm trigger
+# copy the contents of this project to the respective directories
